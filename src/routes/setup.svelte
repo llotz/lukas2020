babc9c39d858b96d79a9c874b59fa663604f3da5
Lukas Lotz <11498557+llotz@users.noreply.github.com>
2020-07-03 13:02:47 +0200

    added image to setup page

--- src/routes/setup.svelte
+++ src/routes/setup.svelte
@@ -3,7 +3,16 @@
 </script>
 
 <style>
-
+  img {
+    max-width: 100%;
+    margin-top: 2em;
+  }
+  h3 {
+    margin-top: 0.5em;
+  }
+  p {
+    margin-left: 0.5em;
+  }
 </style>
 
 <svelte:head>
@@ -12,18 +21,24 @@
 </svelte:head>
 
 <div class="content">
-  <h2>Mein Setup</h2>
-  <h3>PC</h3>
+  <h2>Computer</h2>
   <p>Mainboard: B450 M</p>
   <p>Prozessor: Ryzen 5 2600</p>
   <p>RAM: 32GB DDR4</p>
   <p>Grafik: Gigabyte GeForce 1060 6GB</p>
+
+  <h2>Peripherie</h2>
+
+  <img src="keeb-min.jpg" alt="my keeb" />
 
   <h3>Tastatur</h3>
   <p>Sharkoon Purewriter TKL</p>
 
   <h3>Maus</h3>
   <p>Titanwolf - Gauntlet Wired Gaming Mouse</p>
+
+  <h3>Mauspad</h3>
+  <p>JIALONG Gaming Mauspad XXL</p>
 
   <h3>Monitor</h3>
   <p>27" Curved Samsung</p>
